d24488d7b963cf85d1d347f89c5efefdab4c31f2
Joshua Kidd <kidd.josh.343@gmail.com>
2015-06-26 21:35:36 -0600

    Added post serializer and setup DS RESTAdapter with ember-data

--- admin/app/routes/posts.js
+++ admin/app/routes/posts.js
@@ -1,5 +1,7 @@
+import Ember from 'ember';
+
 export default Ember.Route.extend({
   model: function() {
-    return $.getJSON("http://localhost:3000/api/posts");
+    return this.store.find('post');
   }
 });
