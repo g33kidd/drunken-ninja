74dafd2dace2e2dfff7e3b0ee2a88af91a0b66b7
Joshua Kidd <kidd.josh.343@gmail.com>
2015-06-29 11:06:46 -0600

    Moved middleware compression to middleware file

--- core/server/index.js
+++ core/server/index.js
@@ -1,15 +1,12 @@
 var express     = require('express');
 var middleware  = require('./middleware');
-var compress    = require('compression');
 var Server      = require('./server');
 
 function init() {
   var rootApp = express(),
       adminApp = express();
 
-  // Use gzip compression by default
-  rootApp.use(compress());
-
+  // Setup the middleware and create new Server instance.
   middleware(rootApp, adminApp);
   return new Server(rootApp);
 }
--- core/server/middleware.js
+++ core/server/middleware.js
@@ -1,5 +1,6 @@
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
+var compress        = require('compression');
 var logger          = require('morgan');
 var routes          = require('./routes');
 
@@ -8,16 +9,23 @@ All of this should be moved to a different folder eventaully when more
 middleware is created.
 */
 
+updateActiveTheme = function(req, res, next) {
+  // updates the theme based on the settings for each request.
+  // It should get the current theme and set HBS views directory to
+  // the current theme directory. Same for partials and assets.
+  next();
+}
+
 // Middleware sets up Routes, Express Middleware, HBS, etc..
 setupMiddleware = function setupMiddleware(rootApp, adminApp) {
-
+  rootApp.use(compress());
   rootApp.use(logger('dev'));
-
   adminApp.use(routes.admin);
 
   rootApp.enable('trust proxy');
   rootApp.use(bodyParser.json());
   rootApp.use(bodyParser.urlencoded({ extended: true }));
+  rootApp.use(methodOverride());
 
   rootApp.use('/admin', adminApp);
   rootApp.use(routes.apiBaseUri, routes.api);
